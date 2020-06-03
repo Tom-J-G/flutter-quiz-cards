679255821ff00f2852d14e9554c9b7d1db6c672a
Thomas <thomas.j.goodhew@gmail.com>
2020-06-03 20:27:59 +0100

    card styling

--- lib/screens/quizCard.dart
+++ lib/screens/quizCard.dart
@@ -28,17 +28,18 @@ class _QuizCard extends State<QuizCard> {
   @override
   Widget build(BuildContext context) {
     Container front = Container(
+      margin: const EdgeInsets.all(20),
       width: 250,
-      height: 350,
+      height: 380,
       padding: const EdgeInsets.all(20),
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: <Widget>[
-          Text(widget.question, style: TextStyle(color: Colors.black45)),
-          SizedBox(height:10),
+          Text(widget.question, style: TextStyle(color: Colors.black45, fontSize: 18), textAlign: TextAlign.center,),
+          SizedBox(height:20),
           Column(
             children: widget.possibleAnswers.map(
-              (x) => Text(x , style: TextStyle(color: Colors.black45))
+              (x) => Text(x , style: TextStyle(color: Colors.black45), textAlign: TextAlign.center,)
             ).toList(),
           )
         ]
@@ -47,10 +48,10 @@ class _QuizCard extends State<QuizCard> {
 
     Container back = Container(
       width: 250,
-      height: 350,
+      height: 380,
       padding: const EdgeInsets.all(10),
       child: Center(
-        child: Text(widget.answer, style: TextStyle(color: Colors.black45))
+        child: Text(widget.answer, style: TextStyle(color: Colors.black45, fontSize: 18), textAlign: TextAlign.center,)
       ),
     );
     return GestureDetector(
@@ -58,7 +59,13 @@ class _QuizCard extends State<QuizCard> {
       child: FlippableBox(
         front: front,
         back: back,
-        bg: BoxDecoration(color: Colors.white),
+        bg: BoxDecoration(color: Colors.white,
+          borderRadius: BorderRadius.circular(20),
+          border: Border.all(
+            color: Colors.grey.withOpacity(0.7),
+            width: 2,
+          )
+        ),
         isFlipped: _isFlipped,
     )
     );
